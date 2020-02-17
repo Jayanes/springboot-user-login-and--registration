3d5fa51c14dadb7945335a390eb52c9b2b09729a
jayanes <sebastiyanjayanes@gmail.com>
2020-02-17 18:15:49 +0530

    initial commit

---
+++ src/main/java/com/jayanes/usermanage/UsermanageApplication.java
@@ -0,0 +1,16 @@
+package com.jayanes.usermanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanageApplication.class, args);
+		System.out.println("<<<<<<<<<<<<<<<<< Application Start >>>>>>>>>>>>>>>>>>");
+
+
+	}
+
+}
